fe3f222e2a267a25f2fe99c76a36093bbded547a
itoalberti <itoalberti@gmail.com>
2024-04-03 14:54:01 -0300

    updates

---
+++ backend-bancoprata/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import session from 'express-session';
+// import rotaCliente from './Rotas/rotaCliente.js';
+// import rotaAgencia from './Rotas/rotaAgencia.js';
+// import rotaProduto from './Rotas/rotaProduto.js';
+// import rotaLogin from './Rotas/rotaLogin.js';
+// import { verificarAcesso } from './Seguranca/Autenticacao.js';
+
+console.log('dotenv.config():');
+dotenv.config();
+console.log('process.env():');
+console.log(process.env);
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SEGREDO,
+    resave: false,
+    saveUninitialized: true,
+    maxAge: 1000 * 60 * 5,
+  })
+);
+
+// app.use('/login', rotaLogin);
+// app.use('/agencia', /* verificarAcesso,*/ rotaAgencia);
+// app.use('/cliente', /*/verificarAcesso,*/ rotaCliente);
+// app.use('/produto', /*/verificarAcesso,*/ rotaProduto);
+
+const hostname = '0.0.0.0';
+const porta = 4000;
+
+app.listen(porta, hostname, () => {
+  console.log(`Backend ouvindo em http://${hostname}:${porta}`);
+});
