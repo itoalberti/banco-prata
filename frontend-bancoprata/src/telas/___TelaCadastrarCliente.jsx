d9c538bcf8801d220e7cefc2882dbbc332ce68d2
Maurício <93089055+itoalberti@users.noreply.github.com>
2024-04-24 17:51:25 -0300

    criação tela de cadastro e de exibição de clientes

--- frontend-bancoprata/src/telas/___TelaCadastrarCliente.jsx
+++ frontend-bancoprata/src/telas/___TelaCadastrarCliente.jsx
@@ -2,37 +2,37 @@ import { Button, Col, Dropdown, Form, Row } from 'react-bootstrap';
 import Pagina from '../templates/Pagina';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useState } from 'react';
-import { port, hostname } from '../dados/dados.js';
 
-// const urlAgencia = `http://${hostname}:${port}/agencia`;
-
-export default function TelaCadastrarAgencia(props) {
+export default function TelaCadastrarCliente(props) {
   const [validado, setValidado] = useState(false);
-  const [agencia, setAgencia] = useState({
-    cod_ag: '',
+  const [cliente, setCliente] = useState({
+    cod_cli: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    telefone: '',
     endereco: '',
     cidade: '',
     uf: '',
-    // produtos
+    cod_ag: 0,
   });
 
   function manipularMudanca(e) {
     const elemForm = e.currentTarget;
     const id = elemForm.id;
     const valor = elemForm.value;
-    setAgencia({ ...agencia, [id]: valor });
+    setCliente({ ...cliente, [id]: valor });
   }
 
   function manipulaSubmissao(e) {
     const form = e.currentTarget;
-    // checar outra opção de validação (fetch urlAgencia)
-    if (form.checkValidity() && agencia.uf !== '') {
+    if (form.checkValidity()) {
       // dados válidos → proceder com o cadastro
-      let agencias = props.listaAgencias;
-      agencias.push(agencia);
-      props.setAgencia(agencias);
+      let clientes = props.listaClientes;
+      clientes.push(cliente);
+      props.setCliente(clientes);
       setValidado(false);
-      props.exibirTabela(true);
+      // props.exibirTabela(true);
     } else {
       setValidado(true);
     }
@@ -43,27 +43,52 @@ export default function TelaCadastrarAgencia(props) {
   return (
     <>
       <Pagina>
-        <h2>Cadastro de nova agência</h2>
+        <h2>Cadastro de novo cliente</h2>
         <br />
         <Form noValidate validated={validado} onSubmit={manipulaSubmissao}>
+          {/********************** NOME *********************/}
+          <Form.Group className='mb-3' controlId='nome' style={{ width: '340px' }}>
+            <Form.Label>Nome:</Form.Label>
+            <Form.Control required type='text' id='nome' value={cliente.nome} onChange={manipularMudanca} />
+            <Form.Control.Feedback type='invalid'>Informe o nome do cliente!</Form.Control.Feedback>
+          </Form.Group>
+          {/********************** CPF *********************/}
+          <Form.Group className='mb-3' controlId='cpf' style={{ width: '340px' }}>
+            <Form.Label>CPF:</Form.Label>
+            <Form.Control required type='text' id='cpf' value={cliente.cpf} onChange={manipularMudanca} />
+            <Form.Control.Feedback type='invalid'>Informe o CPF do cliente!</Form.Control.Feedback>
+          </Form.Group>
+          {/********************** EMAIL *********************/}
+          <Form.Group className='mb-3' controlId='email' style={{ width: '340px' }}>
+            <Form.Label>Email:</Form.Label>
+            <Form.Control required type='email' id='email' value={cliente.email} onChange={manipularMudanca} />
+            <Form.Control.Feedback type='invalid'>Informe o email do cliente!</Form.Control.Feedback>
+          </Form.Group>
+          {/********************** TELEFONE *********************/}
+          <Form.Group className='mb-3' controlId='telefone' style={{ width: '340px' }}>
+            <Form.Label>Email:</Form.Label>
+            <Form.Control required type='text' id='telefone' value={cliente.telefone} onChange={manipularMudanca} />
+            <Form.Control.Feedback type='invalid'>Informe o telefone do cliente!</Form.Control.Feedback>
+          </Form.Group>
           {/********************** ENDEREÇO *********************/}
           <Form.Group className='mb-3' controlId='endereco' style={{ width: '340px' }}>
             <Form.Label>Endereço:</Form.Label>
-            <Form.Control required type='text' id='endereco' value={agencia.endereco} onChange={manipularMudanca} />
+            <Form.Control required type='text' id='endereco' value={cliente.endereco} onChange={manipularMudanca} />
             <Form.Control.Feedback type='invalid'>Informe o endereço da agência!</Form.Control.Feedback>
           </Form.Group>
-
           {/********************** CIDADE **********************/}
           <Form.Group className='mb-3' controlId='cidade' style={{ width: '340px' }}>
             <Form.Label>Cidade:</Form.Label>
-            <Form.Control required type='text' id='cidade' value={agencia.cidade} onChange={manipularMudanca} />
+            <Form.Control required type='email' id='cidade' value={cliente.cidade} onChange={manipularMudanca} />
             <Form.Control.Feedback type='invalid'>Informe a cidade da agência!</Form.Control.Feedback>
           </Form.Group>
-
+          {/* <Row> */}
           {/********************** UF **********************/}
+          {/* <Col md='2'> */}
           <Form.Group className='mb-3' controlId='uf'>
             <Form.Label>UF:</Form.Label>
-            <Form.Select required id='uf' style={{ width: '100px' }}>
+            {/* <Form.Label style={{ width: '50px' }}>UF:</Form.Label> */}
+            <Form.Select className='mb-3' style={{ width: '100px' }} id='uf'>
               <option value=''></option>
               <option value='AC'>AC</option>
               <option value='AL'>AL</option>
@@ -93,47 +118,17 @@ export default function TelaCadastrarAgencia(props) {
               <option value='TO'>TO</option>
               <option value='DF'>DF</option>
             </Form.Select>
-            {/* <Dropdown.Toggle required id='uf'>
-                  akjsdh
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href='AC'>AC</Dropdown.Item>
-                  <Dropdown.Item>AL</Dropdown.Item>
-                  <Dropdown.Item>AP</Dropdown.Item>
-                  <Dropdown.Item>AM</Dropdown.Item>
-                  <Dropdown.Item>BA</Dropdown.Item>
-                  <Dropdown.Item>CE</Dropdown.Item>
-                  <Dropdown.Item>ES</Dropdown.Item>
-                  <Dropdown.Item>GO</Dropdown.Item>
-                  <Dropdown.Item>MA</Dropdown.Item>
-                  <Dropdown.Item>MT</Dropdown.Item>
-                  <Dropdown.Item>MS</Dropdown.Item>
-                  <Dropdown.Item>MG</Dropdown.Item>
-                  <Dropdown.Item>PA</Dropdown.Item>
-                  <Dropdown.Item>PB</Dropdown.Item>
-                  <Dropdown.Item>PR</Dropdown.Item>
-                  <Dropdown.Item>PE</Dropdown.Item>
-                  <Dropdown.Item>PI</Dropdown.Item>
-                  <Dropdown.Item>RJ</Dropdown.Item>
-                  <Dropdown.Item>RN</Dropdown.Item>
-                  <Dropdown.Item>RS</Dropdown.Item>
-                  <Dropdown.Item>RO</Dropdown.Item>
-                  <Dropdown.Item>RR</Dropdown.Item>
-                  <Dropdown.Item>SC</Dropdown.Item>
-                  <Dropdown.Item>SP</Dropdown.Item>
-                  <Dropdown.Item>SE</Dropdown.Item>
-                  <Dropdown.Item>TO</Dropdown.Item>
-                  <Dropdown.Item>DF</Dropdown.Item>
-                </Dropdown.Menu> */}
             <Form.Control.Feedback type='invalid'>Informe o estado da agência!</Form.Control.Feedback>
           </Form.Group>
+          {/* </Col> */}
+          {/* </Row> */}
 
           <br />
           <Row>
             {/* BOTÃO DE CADASTRAR */}
             <Col xs='auto'>
-              <Button variant='primary' type='submit'>
-                Cadastrar agência
+              <Button variant='dark' type='submit'>
+                Cadastrar cliente
               </Button>
             </Col>
 
